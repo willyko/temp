94189274739f3ba271d7436179a89374a38925bd
sidhujag <sidhujag@gmail.com>
2019-01-04 16:36:40 -0800

    remove file

--- confdefs.h
+++
@@ -1,8 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "Syscoin Core"
-#define PACKAGE_TARNAME "syscoin"
-#define PACKAGE_VERSION "4.0.0"
-#define PACKAGE_STRING "Syscoin Core 4.0.0"
-#define PACKAGE_BUGREPORT "https://github.com/syscoin/syscoin/issues"
-#define PACKAGE_URL "https://syscoin.org/"
-#define HAVE_CXX11 1

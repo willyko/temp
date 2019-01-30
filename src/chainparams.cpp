915e8a5c3bca4b69f1641ce66230103d87c071f6
sidhujag <sidhujag@gmail.com>
2019-01-29 16:44:34 -0800

    comment out seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -171,10 +171,11 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed1.syscoin.org");
+        // TODO re-enable once we go live
+       /* vSeeds.emplace_back("seed1.syscoin.org");
         vSeeds.emplace_back("seed2.syscoin.org");
         vSeeds.emplace_back("seed3.syscoin.org");
-        vSeeds.emplace_back("seed4.syscoin.org");
+        vSeeds.emplace_back("seed4.syscoin.org");*/
         
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,63);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);

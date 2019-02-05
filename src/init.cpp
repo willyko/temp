ed6b4c799f1661df2e997980bbf493f427128cc5
sidhujag <sidhujag@gmail.com>
2019-02-04 16:17:08 -0800

    update to 3.9gb

--- src/init.cpp
+++ src/init.cpp
@@ -1835,14 +1835,14 @@ bool AppInitMain()
         LogPrintf("MASTERNODE:\n");
         meminfo_t memInfo = parse_meminfo();
         LogPrintf("Total Memory(MB) %d (Total Free %d) Swap Total(MB) %d (Total Free %d)\n", memInfo.MemTotalMiB, memInfo.MemAvailableMiB, memInfo.SwapTotalMiB, memInfo.SwapFreeMiB);
-        if(memInfo.MemTotalMiB < 4000)
+        if(memInfo.MemTotalMiB < 3900)
             return InitError(_("Insufficient memory, you need atleast 4GB RAM to run a masternode and be running in a Unix OS. Please see documentation."));
-        if(memInfo.MemTotalMiB < 8000 && memInfo.SwapTotalMiB < 4000)
+        if(memInfo.MemTotalMiB < 7900 && memInfo.SwapTotalMiB < 3900)
             return InitError(_("Insufficient swap memory, you need atleast 4GB swap RAM to run a masternode and be running in a Unix OS. Please see documentation."));           
         LogPrintf("Total number of physical cores found %d\n", boost::thread::physical_concurrency());
         if(boost::thread::physical_concurrency() < 2)
             return InitError(_("Insufficient CPU cores, you need atleast 2 cores to run a masternode. Please see documentation."));
-
+            
         bool isRunning = (0 == ::system("pgrep syscoind > /dev/null"));
         if(isRunning)
             return InitError(_("Only one syscoind instance is allowed to run.")); 

0cfcf7d8e53bc3e94e7dfaaedc3c20a4a7aadde4
Willy Ko <wko@blockchainfoundry.co>
2019-02-08 14:35:30 -0800

    Added rinkeby bootnode for starting geth testnet

--- src/util.cpp
+++ src/util.cpp
@@ -1133,7 +1133,12 @@ bool StartGethNode(pid_t &pid, bool bGethTestnet, int websocketport)
         if( pid == 0 ) {
             std::string portStr = std::to_string(websocketport);
             if(bGethTestnet){
-                char * argv[] = {(char*)fpath.c_str(), (char*)"--rpc", (char*)"--rinkeby", (char*)"--rpccorsdomain", (char*)"*", (char*)"--rpcapi", (char*)"eth,net,web3,admin", (char*)"--ws", (char*)"--wsport", (char*)portStr.c_str(), (char*)"--wsorigins", (char*)"*", (char*)"--syncmode", (char*)"light", NULL };
+                char * argv[] = {(char*)fpath.c_str(), (char*)"--rpc", (char*)"--rinkeby", 
+                    (char*)"--rpccorsdomain", (char*)"*", (char*)"--rpcapi", (char*)"eth,net,web3,admin",
+                    (char*)"--ws", (char*)"--wsport", (char*)portStr.c_str(), 
+                    (char*)"--wsorigins", (char*)"*",
+                    (char*)"--syncmode", (char*)"light", 
+                    (char*)"--bootnodes", (char*)"enode://a24ac7c5484ef4ed0c5eb2d36620ba4e4aa13b8c84684e1b4aab0cebea2ae45cb4d375b77eab56516d34bfbd3c1a833fc51296ff084b770b94fb9028c4d25ccf@52.169.42.101:30303", NULL };
                 execvp(argv[0], &argv[0]);
             }
             else{
@@ -1147,9 +1152,10 @@ bool StartGethNode(pid_t &pid, bool bGethTestnet, int websocketport)
         }
     #else
         std::string portStr = std::to_string(websocketport);
-        std::string args = std::string("--rpc --rpccorsdomain * --rpcapi eth,net,web3,admin --ws --wsport ") + portStr + std::string(" --wsorigins * --syncmode light");
+        std::string args = std::string("--rpc --rpccorsdomain * --rpcapi eth,net,web3,admin --ws --wsport ") + portStr + std::string(" --wsorigins * --syncmode light") 
         if(bGethTestnet)
             args += std::string(" --rinkeby");
+            args += std::string(" --bootnodes enode://a24ac7c5484ef4ed0c5eb2d36620ba4e4aa13b8c84684e1b4aab0cebea2ae45cb4d375b77eab56516d34bfbd3c1a833fc51296ff084b770b94fb9028c4d25ccf@52.169.42.101:30303");
         pid = fork(fpath.string(), args);
         if( pid <= 0 ) {
             LogPrintf("Could not start Geth\n");
